b44ebb1cce23a92b757187cd7622a7ad0f9229b6
KKiiim <jerryfoooster@gmail.com>
2024-12-30 13:43:12 +0800

    feat: imply wasm and arm64 func `sum` `fibonacci` in chapter0 (#2)

---
+++ src/Chapter00/start_arm64/func.h
@@ -0,0 +1,7 @@
+#pragma once
+
+extern "C" {
+#include <stdint.h>
+uint64_t mySum(uint32_t n);
+uint64_t myFibo(uint32_t n);
+}
---
+++ src/Chapter00/start_arm64/sum_fibo.s
@@ -0,0 +1,63 @@
+.text
+.global mySum, myFibo
+
+mySum:
+    stp x29, x30, [sp, -32]!    
+    mov x29, sp 
+    stp x1, x2, [sp, 16] 
+    # x0 is input n
+    # x1 store loop i
+    # x2 store sum result
+    mov x1, #0
+    mov x2, #0
+inner_loop:
+    add x2, x1, x2
+    add x1, x1, #1
+    cmp x1, x0
+    ble inner_loop
+
+    mov x0, x2
+    ldp x1, x2, [sp, 16]
+    ldp x29, x30, [sp], 32 
+    ret
+
+myFibo:
+    # x0 is input n, caller stored
+
+    # ignore use register for func-call optimize, just store in stack
+
+    # store caller's sp, lr
+    stp x29, x30, [sp, -32]!    
+    # sp = sp -32, store 4 8B number: x29(sp), x30(lr), x19(tmp), x0
+    mov x29, sp 
+    # x29 store the current stack pointer
+    stp x19, x0, [sp, 16] 
+    # x19, x0 stored for this stackFrame use 
+
+    cmp x0, 0
+    beq prepare_ret
+    cmp x0, 1
+    beq prepare_ret
+
+    sub x0, x0, #1
+    bl myFibo 
+    # bl store returnAddress in x30
+    # x0 is ret
+    str x0, [sp, 16] 
+    # store fibo(n-1) in x19
+
+    ldr x0, [sp, 24]
+    sub x0, x0, #2
+    bl myFibo
+    # x0 is fibo(n-2)
+    ldr x19, [sp, 16]
+    add x0, x0, x19 
+    # x0 = fibo(n-1) + fibo(n-2)
+
+prepare_ret:
+    ldr x19, [sp, 16]
+    # no need to change x0, x0 is set before prepare_ret
+    ldp x29, x30, [sp], 32 
+    # sp += 32
+    ret
+    
